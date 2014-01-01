5c00c23ffad04aaf1c0c56c21b67dfab785094fe
Bastin Robin <bastinrobins@gmail.com>
2014-01-01 19:38:27 +0530

    Minor tweaks

--- js/chart.js
+++ js/chart.js
@@ -6,82 +6,82 @@ var treemap = function(data, conf) {
 
   //Color scale define
   // var colorScale = d3.scale.category20c();
- var amount = d3.extent(_.pluck(data, conf['color']));
+   var amount = d3.extent(_.pluck(data, conf['color']));
 
- //Custom color scale
+  //Custom color scale
   var colorScale = d3.scale.linear()
-    .clamp(true)
-    .domain([amount[0],(amount[0] + amount[1]) / 2 ,amount[1]])
-    .range(['#756bb1', '#bcbddc', '#efedf5']);
+  .clamp(true)
+  .domain([amount[0],(amount[0] + amount[1]) / 2 ,amount[1]])
+  // .range(['#756bb1', '#bcbddc', '#efedf5']);
+  .range(['#efedf5', '#bcbddc', '#756bb1']);
 
   if(conf['body']) {
-    //Data nesting
-     root = {  values: d3.nest()
-              .key(function(d) {  return d[conf['head']]; })
-              .key(function(d) {  return d[conf['body']]; })
-              .rollup(function(d) { return d.map(function(d) { return { key: parseInt(d[conf['size']]) }; }); })
-              .entries(data) };
+  //Data nesting
+  root = {  values: d3.nest()
+          .key(function(d) {  return d[conf['head']]; })
+          .key(function(d) {  return d[conf['body']]; })
+          .rollup(function(d) { return d.map(function(d) { return { key: parseInt(d[conf['size']]) }; }); })
+          .entries(data) };
   } else {
-     //Data nesting
-      root = {  values: d3.nest()
-               .key(function(d) {  return d[conf['head']]; })
-               .rollup(function(d) { return d.map(function(d) { return { key: parseInt(d[conf['size']]) }; }); })
-               .entries(data) };
+  //Data nesting
+  root = {  values: d3.nest()
+           .key(function(d) {  return d[conf['head']]; })
+           .rollup(function(d) { return d.map(function(d) { return { key: parseInt(d[conf['size']]) }; }); })
+           .entries(data) };
   }
-  
+
   //Plot the data as treemap
   var treemap = d3.layout.treemap()
-        .size([w, h])
-        .sticky(true)
-        .padding(1)
-        .sort(function(a,b) { return a.value - b.value; })
-        .children(function(d) { return d.values; }) 
-        .value(function(d) { return d.key; }); 
+    .size([w, h])
+    .sticky(true)
+    .padding(1)
+    .sort(function(a,b) { return a.value - b.value; })
+    .children(function(d) { return d.values; }) 
+    .value(function(d) { return d.key; }); 
 
 
-      var svg = d3.select(".chart")
-        .append("svg")
-        .style("width", w + "px")
-        .style("height", h + "px")
-        .attr("transform", "translate(.5,.5)")
-        .on('click', secondLevel);
+  var svg = d3.select(".chart")
+    .append("svg")
+    .style("width", w + "px")
+    .style("height", h + "px")
+    .attr("transform", "translate(.5,.5)")
+    .on('click', secondLevel);
 
-      var cell = svg.selectAll(".cell")
-              .data(treemap.nodes(root).filter(function(d) { return d.values; }) )
-            
-      // enter new elements 
-      var cellEnter = cell.enter()
-          .append("g") 
-          .attr("class", "cell");    
-      cellEnter.append("rect");   
-      cellEnter.append("text"); 
-            
-      // update remaining elements 
-      cell.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-      
-      //Append rectangle
-      cell.select("rect") 
-            .attr("width", function (d) { return d.dx; })
-            .attr("height", function (d) { return d.dy; })
-            .attr('data-title', function (d, i) { 
-              console.log(d);
-              return d.key+','+d.value+','+'Infraction '+data[i]['time_of_infraction']+' Time'; })
-            .style("fill", function(d) { return colorScale(d.value); });
+  var cell = svg.selectAll(".cell")
+          .data(treemap.nodes(root).filter(function(d) { return d.values; }) )
+        
+  // enter new elements 
+  var cellEnter = cell.enter()
+      .append("g") 
+      .attr("class", "cell");    
+  cellEnter.append("rect");   
+  cellEnter.append("text"); 
+        
+  // update remaining elements 
+  cell.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-      //Append text
-      cell.select("text")
-          .attr("x", function(d) { return d.dx / 2; })
-          .attr("y", function(d) { return d.dy / 2; })
-          .attr("dy", ".35em")  
-          .attr("text-anchor", "middle")
-          .style('font-size', function(d) { return d.dx / 10 + "px"; })
-         .text(function(d) { return d.key; }) 
+  //Append rectangle
+  cell.select("rect") 
+        .attr("width", function (d) { return d.dx; })
+        .attr("height", function (d) { return d.dy; })
+        .attr('data-title', function (d, i) { 
+          return d.key+','+d.value+','+'Infraction '+data[i]['time_of_infraction']+' Time'; })
+        .style("fill", function(d, i) { return colorScale(data[i][conf['color']]); });
 
-      $('[data-title]').tooltip({
-              container: 'body',
-              html: true,
-              placement: 'right'
-          });
+  //Append text
+  cell.select("text")
+      .attr("x", function(d) { return d.dx / 2; })
+      .attr("y", function(d) { return d.dy / 2; })
+      .attr("dy", ".35em")  
+      .attr("text-anchor", "middle")
+      .style('font-size', function(d) { return d.dx / 10 + "px"; })
+     .text(function(d) { return d.key; }) 
+
+  $('[data-title]').tooltip({
+          container: 'body',
+          html: true,
+          placement: 'right'
+  });
 }
 
 // Second Level Treemap
@@ -99,6 +99,22 @@ $(document).ready(function() {
     // plot treemap
     localStorage.setItem('data', JSON.stringify(data));
     treemap(data, {head: 'location1', size: 'set_fine_amount'});
+
+    //Get the column headers
+    var columns = d3.keys(data[0]);
+    console.log(columns);
+    //Load Nav Bar contents
+    d3.select('.metric-dropdown').selectAll('li')
+       .data(columns)
+      .enter()
+        .append('li')
+        .append('a')
+            .attr('href', '#')
+            .text(String)
+            .on('click', function(column) {
+                d3.event.preventDefault();
+                location.hash = column;
+            });
 
   });
 
@@ -121,5 +137,10 @@ $(document).ready(function() {
 
 
     add_search('.search', '.chart');
+
+    // Reload trigger
+    $('.reload').click(function() {
+        location.reload();
+    });
 });
 
